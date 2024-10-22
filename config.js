a88cbf64a42688d7309d10b097c295abaea813d2
shawarma-pixle <kakasibesnis@gmail.com>
2024-10-22 20:21:02 +0300

    config.js

--- config.js
+++ config.js
@@ -52,7 +52,7 @@ global.confirmCode = "" // No tocar esto : Do not touch this line
 // ❰❰ IDIOMAS DISPONIBLES : AVAILABLE LANGUAGES ❱❱
 // Español 👉 es           
 // English 👉 en
-global.lenguajeGB = es
+global.lenguajeGB = ar
 global.mid = esDefault
 global.version_language = '1.0 (MID-GB)'
 
